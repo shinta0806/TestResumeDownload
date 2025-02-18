286b801409a62863e32962654b021bbc045d52d5
shinta0806 <shinta.0806@gmail.com>
2025-02-18 23:45:43 +0900

    ダウンロードを実装。

--- TestResumeDownload/ViewModels/MainWindows/MainPageViewModel.cs
+++ TestResumeDownload/ViewModels/MainWindows/MainPageViewModel.cs
@@ -9,8 +9,10 @@
 // ----------------------------------------------------------------------------
 
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 namespace TestResumeDownload.ViewModels.MainWindows;
 
@@ -25,6 +27,8 @@ internal partial class MainPageViewModel : ObservableRecipient
 	/// </summary>
 	public MainPageViewModel()
 	{
+		// コマンド
+		ButtonDownloadClickedCommand = new(ButtonDownloadClicked);
 	}
 
 	// ====================================================================
@@ -58,12 +62,56 @@ internal partial class MainPageViewModel : ObservableRecipient
 	/// <summary>
 	/// ダウンロードサイズ [MB]
 	/// </summary>
-	private Int32 _downloadSize;
+	private Int32 _downloadSize = 5;
 	public Int32 DownloadSize
 	{
 		get => _downloadSize;
 		set => SetProperty(ref _downloadSize, value);
 	}
+
+	/// <summary>
+	/// コントロールが有効か
+	/// </summary>
+	private Boolean _isControlEnabled = true;
+	public Boolean IsControlEnabled
+	{
+		get => _isControlEnabled;
+		set => SetProperty(ref _isControlEnabled, value);
+	}
+
+	// --------------------------------------------------------------------
+	// コマンド
+	// --------------------------------------------------------------------
+
+	#region ダウンロードボタンの制御
+	public RelayCommand ButtonDownloadClickedCommand
+	{
+		get;
+	}
+
+	private async void ButtonDownloadClicked()
+	{
+		try
+		{
+			IsControlEnabled = false;
+			CheckInput();
+			Exception? ex = await DownloadAsync();
+			if (ex != null)
+			{
+				throw ex;
+			}
+			await ShowContentDialogAsync("完了", "完了");
+		}
+		catch (Exception ex)
+		{
+			await ShowContentDialogAsync("エラー", ex.Message);
+		}
+		finally
+		{
+			IsControlEnabled = true;
+		}
+	}
+	#endregion
 
 	// ====================================================================
 	// public 関数
@@ -80,4 +128,111 @@ internal partial class MainPageViewModel : ObservableRecipient
 		App.MainWindow.Height = 240;
 	}
 
+	// ====================================================================
+	// private 変数
+	// ====================================================================
+
+	/// <summary>
+	/// HTTP クライアント
+	/// </summary>
+	private static readonly HttpClient _client = new();
+
+	// ====================================================================
+	// private 関数
+	// ====================================================================
+
+	/// <summary>
+	/// 入力チェック
+	/// </summary>
+	private void CheckInput()
+	{
+		if (Url.Length <= "https://".Length)
+		{
+			throw new Exception("URL を入力してください。");
+		}
+		if (String.IsNullOrEmpty(DestFolder))
+		{
+			throw new Exception("ダウンロードフォルダーを入力してください。");
+		}
+		Directory.CreateDirectory(DestFolder);
+		if (DownloadSize <= 0)
+		{
+			throw new Exception("ダウンロードサイズを入力してください。");
+		}
+	}
+
+	/// <summary>
+	/// ダウンロード
+	/// </summary>
+	/// <returns></returns>
+	private async Task<Exception?> DownloadAsync()
+	{
+		return await Task.Run(() =>
+		{
+			try
+			{
+				Int64 existingSize = 0;
+				if (File.Exists(DestPartialPath()))
+				{
+					existingSize = new FileInfo(DestPartialPath()).Length;
+				}
+
+				using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, Url);
+				request.Headers.Range = new(existingSize, null);
+				using HttpResponseMessage response = _client.Send(request, HttpCompletionOption.ResponseHeadersRead);
+				response.EnsureSuccessStatusCode();
+
+				using FileStream fileStream = new FileStream(DestPartialPath(), FileMode.Append, FileAccess.Write, FileShare.None);
+				using Stream contentStream = response.Content.ReadAsStream();
+				Byte[] buffer = new Byte[2048];
+				Int32 bytesRead;
+
+				while ((bytesRead = contentStream.Read(buffer, 0, buffer.Length)) > 0)
+				{
+					fileStream.Write(buffer, 0, bytesRead);
+				}
+				return null;
+			}
+			catch (Exception ex)
+			{
+				return ex;
+			}
+		});
+	}
+
+	/// <summary>
+	/// ダウンロード中の一時ファイルパス
+	/// </summary>
+	/// <returns></returns>
+	private String DestPartialPath()
+	{
+		return DestPath() + ".part";
+	}
+
+	/// <summary>
+	/// 最終ダウンロードパス
+	/// </summary>
+	/// <returns></returns>
+	private String DestPath()
+	{
+		return Path.Combine(DestFolder, Path.GetFileName(Url));
+	}
+
+	/// <summary>
+	/// ContentDialog を表示
+	/// </summary>
+	/// <param name="title"></param>
+	/// <param name="message"></param>
+	/// <returns></returns>
+	private async Task ShowContentDialogAsync(String title, String message)
+	{
+		ContentDialog contentDialog = new()
+		{
+			Title = title,
+			Content = message,
+			PrimaryButtonText = "OK",
+			XamlRoot = App.MainWindow.Content.XamlRoot,
+		};
+		await contentDialog.ShowAsync();
+	}
 }
